eaf27042520fab2d33b502c77e748c4db9ee9f97
yatkitlau84 <72048862+yatkitlau84@users.noreply.github.com>
2020-09-28 18:35:16 -0400

    Add files via upload

---
+++ animal.py
@@ -0,0 +1,12 @@
+# project-1
+
+
+
+# Author : Lau, Yat Kit
+# Date: 9/28/2020
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
+
